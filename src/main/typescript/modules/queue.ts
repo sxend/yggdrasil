e8bb85b942aa2d46161955e43424c3980c388317
sxend <arimitsu.k+github@gmail.com>
2017-10-27 01:09:30 +0900

    add consume config

--- src/main/typescript/modules/queue.ts
+++ src/main/typescript/modules/queue.ts
@@ -3,18 +3,30 @@ import { Yggdrasil } from '../yggdrasil';
 export interface Queue {
   q: Function[];
 }
-
+const defaults = {
+  'consume-interval': 10,
+  'consume-throughput': 100
+};
 export function queue(ygg: Yggdrasil): void {
   ygg.q = ygg.q || [];
-  function consumeQueue() {
-    while (ygg.q.length > 0) {
+  ygg.configure({
+    queue: defaults
+  });
+  function consume() {
+    let count = 0;
+    while (ygg.q.length > 0 && count < getOrDefault(ygg, 'consume-throughput')) {
       try {
         ygg.q.shift().bind(ygg)(ygg);
       } catch (e) {
         console.error(e);
       }
+      count++;
     }
-    setTimeout(consumeQueue, 10);
+    setTimeout(consume, getOrDefault(ygg, 'consume-interval'));
   }
-  setTimeout(consumeQueue, 10);
+  setTimeout(consume, getOrDefault(ygg, 'consume-interval'));
+}
+
+function getOrDefault(ygg: Yggdrasil, key: string): any {
+  return ygg.configuration().queue[key] || defaults[key];
 }
